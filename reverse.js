299c8face515728396ccc88ddf038ab505ad7cee
Prafullata Sonawane <prafullatass@gmail.com>
2019-07-05 16:39:32 -0500

    Added palindrome function

--- reverse.js
+++ reverse.js
@@ -5,4 +5,14 @@ var str = "this is a test program"
 str = str.split('').reverse().join('');
 console.log(str);
 
-//check for
+//check for palindrome
+//sentence or other type of character sequence which reads the same backward as forward.
+// For example, “racecar” and “Anna” are palindromes.
+
+const palindrome = str => {
+str = str.toLowerCase();
+return str === str.split('').reverse().join('');
+}
+
+console.log(palindrome("Anna"));
+console.log(palindrome("Friend"));
